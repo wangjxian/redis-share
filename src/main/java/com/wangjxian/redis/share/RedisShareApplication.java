b462f7e13392d255239ed34d96119bcaa1e6a41c
wangjxian <wjx0601@163.com>
2019-12-02 13:35:11 +0800

    Initial commit

---
+++ src/main/java/com/wangjxian/redis/share/RedisShareApplication.java
@@ -0,0 +1,13 @@
+package com.wangjxian.redis.share;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisShareApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(RedisShareApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/wangjxian/redis/share/RedisShareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wangjxian.redis.share;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisShareApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
